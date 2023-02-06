9878a313c2400e43d45a3a8c314ea44618844f66
RomPop76 <r.popielas@gmail.com>
2023-02-06 18:31:05 +0100

    Commit 5

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-const currentNumber = document.querySelector('.currentNumber');
+
+ const currentNumber = document.querySelector('.currentNumber');
+
 const previousNumber = document.querySelector('.previousNumber p');
-const mathSign = document.querySelector('mathSign');
-const numberButton = document.querySelectorAll('.number');
-const operatorButtons = document.querySelectorAll('.operator');
+
+const mathSign = document.querySelector('.mathSign');
+
+const numbersButtons = document.querySelectorAll('.number');
+
+const operatorsButtons = document.querySelectorAll('.operator');
+
 const equalsButton = document.querySelector('.equals');
+
 const clearButton = document.querySelector('.clear');
+
 const calculatorHistory = document.querySelector('.history');
-const historyBtn =  document.querySelector('.historyBtn');
+
+const historyBtn = document.querySelector('.history-btn');
+
 
 let result = '';
 
@@ -15,22 +25,61 @@ let result = '';
 
 
 
+function displayNumbers () {
+        if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
+        if(this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '.0'
+
+        currentNumber.innerHTML += this.textContent;
+}
+
+
+
+
+function operate () {
+   if(currentNumber.innerHTML === '' && this.textContent === '-') {
+    currentNumber.innerHTML = '-';
+    return;
+   }
+}
 
 
 
 
 
+function showResult () {
+    
+
+}
+
+function addToHistory () {
+    
+}
+
+
+function clearHistory () {
+
+}
 
 
 
+function clearScreen () {
+   
+
+}
 
 
+ // Nasluchiwanie przyciskow
+
+ operatorsButtons.forEach((button) => button.addEventListener('click', operate))
+
+ equalsButton.addEventListener('click', showResult);
 
 
+ clearButton.addEventListener('click', clearScreen);
+
+ numbersButtons.forEach((button) => {
+     button.addEventListener('click', displayNumbers)
+ })
 
 
-// Nasłuchiwanie przycisków
-
-operatorButtons.forEach((button) => button.addEventListener('click', operate));
-
-equalsButton.addEventListener('click', showResult);
+ historyBtn.addEventListener('click', clearHistory);
